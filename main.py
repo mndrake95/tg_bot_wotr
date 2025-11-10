52f38eb5d63390484f4c7817cc8c3b6e46e37703
mndrake95 <mandra4a@gmail.com>
2025-11-10 13:57:50 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,45 +1,38 @@
-# Импортируем нужные функции
 from dotenv import load_dotenv
 import os
 import telebot
 
 
 
-# Загружаем переменные из файла token.env
 load_dotenv('token.env') 
 
-# Получаем токен из переменных окружения
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
-# Юзаем эту переменную для инициализации бота
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# Словарь для хранения последнего сообщения бота в каждом чате
 last_bot_messages = {}
+
+def delete_last_bot_message(chat_id: int):
+    """Удаляет последнее отправленное ботом сообщение в указанном чате, если оно есть."""
+    if chat_id in last_bot_messages:
+        try:
+            bot.delete_message(chat_id=chat_id, message_id=last_bot_messages[chat_id])
+            # Удаляем из словаря, чтобы не пытаться удалить сообщение снова
+            del last_bot_messages[chat_id]
+        except telebot.apihelper.ApiTelegramException as e:
+            print(f"Failed to delete message: {e}")
 
 @bot.message_handler(commands=['start','hello'])
 def send_welcome(message):
-    chat_id = message.chat.id
-    if chat_id in last_bot_messages:
-        try:
-            bot.delete_message(chat_id=chat_id, message_id=last_bot_messages[chat_id])
-        except telebot.apihelper.ApiTelegramException as e:
-            print(f"Failed to delete message: {e}")
-
-    sent_message = bot.send_message(chat_id, "Здарова, датебайо")
-    last_bot_messages[chat_id] = sent_message.message_id
+    delete_last_bot_message(message.chat.id)
+    sent_message = bot.send_message(message.chat.id, "Здарова, датебайо")
+    last_bot_messages[message.chat.id] = sent_message.message_id
 
 @bot.message_handler(commands=['naruto'])
 def echo_all(message):
-    chat_id = message.chat.id
-    if chat_id in last_bot_messages:
-        try:
-            bot.delete_message(chat_id=chat_id, message_id=last_bot_messages[chat_id])
-        except telebot.apihelper.ApiTelegramException as e:
-            print(f"Failed to delete message: {e}")
-
-    sent_message = bot.send_message(chat_id, "Ты не злой ты не злой, ты хороший ты хороший!")
-    last_bot_messages[chat_id] = sent_message.message_id
+    delete_last_bot_message(message.chat.id)
+    sent_message = bot.send_message(message.chat.id, "Ты не злой ты не злой, ты хороший ты хороший!")
+    last_bot_messages[message.chat.id] = sent_message.message_id
     bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
 
 
